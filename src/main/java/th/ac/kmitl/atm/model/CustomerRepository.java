c2e296a314940783c58ccb81321d5d2945081b34
Siriporn Jansorn <61050891@kmitl.ac.th>
2020-11-15 14:33:38 +0700

    add interface CustomerRepository

--- src/main/java/th/ac/kmitl/atm/model/CustomerRepository.java
+++ src/main/java/th/ac/kmitl/atm/model/CustomerRepository.java
@@ -1,2 +1,8 @@
-package th.ac.kmitl.atm.model;public interface CustomerRepository {
+package th.ac.kmitl.atm.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
 }
